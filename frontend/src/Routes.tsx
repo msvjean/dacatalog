79a28622a1dd21adcbcbacaef2f9f26f27b4899f
Jean Marcel <jean.msv@gmail.com>
2022-04-19 15:02:17 -0300

    ProductDetails route

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import ProductDetails from 'pages/ProductDetails';
 
 const Caminho = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Caminho = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Catalog />} />
+      <Route path="/products/:productId" element={<ProductDetails />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
   </BrowserRouter>
---
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const ProductDetails = () => {
+    return(
+        <div>
+            <h1>Pagina Product Details</h1>
+        </div>
+    );
+};
+
+export default ProductDetails;
