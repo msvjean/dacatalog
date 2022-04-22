64314aca0b6558066e1c2a9331ff1008832e685b
Jean Marcel <jean.msv@gmail.com>
2022-04-22 11:34:36 -0300

    Catalog - ProductDetails links

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,35 +1,45 @@
 import './style.css';
-import {ReactComponent as ArrowImg} from 'assets/images/arrow.svg';
+import { ReactComponent as ArrowImg } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 const ProductDetails = () => {
-    return(
-        <div className="product-details-container">
-            <div className="base-card product-details-card">
-                <div className="goback-container">
-                    <ArrowImg />
-                    <h2>VOLTAR</h2>
-                </div>
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={1234.56}/>
-                        </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, totam laboriosam. Molestias expedita officia illum modi nemo quidem rerum dolorum!</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowImg />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do produto"
+              />
             </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={1234.56} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Eveniet, totam laboriosam. Molestias expedita officia illum modi
+                nemo quidem rerum dolorum!
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
