a2144b3ab1402dd3eaa2b982d4a682ca28960534
Jean Marcel <jean.msv@gmail.com>
2022-04-26 18:18:55 -0300

    Catalog, ProductDatails loader

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -7,11 +7,13 @@ import { AxiosParams } from 'types/vendor/axios';
 import { SpringPage } from 'types/vendor/spring';
 import { BASE_URL } from 'util/requests';
 import ProductCard from '../../components/ProductCard';
+import CardLoader from './CardLoader';
 
 import './style.css';
 
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoding] = useState(false);
 
   useEffect(() => {
     const params: AxiosParams = {
@@ -23,9 +25,14 @@ const Catalog = () => {
       },
     };
 
-    axios(params).then((response) => {
-      setPage(response.data);
-    });
+    setIsLoding(true);
+    axios(params)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .finally(() => {
+        setIsLoding(false);
+      });
   }, []);
 
   return (
@@ -34,13 +41,17 @@ const Catalog = () => {
         <h1>Catálogo de produtos</h1>
       </div>
       <div className="row">
-        {page?.content.map((product) => (
-          <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
-            <Link to={`/products/${product.id}`}>
-              <ProductCard product={product} />
-            </Link>
-          </div>
-        ))}
+        {isLoading ? (
+          <CardLoader />
+        ) : (
+          page?.content.map((product) => (
+            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+              <Link to={`/products/${product.id}`}>
+                <ProductCard product={product} />
+              </Link>
+            </div>
+          ))
+        )}
       </div>
       <div className="row">
         <Pagination />
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -6,24 +6,29 @@ import { useEffect, useState } from 'react';
 import { Product } from 'types/product';
 import axios from 'axios';
 import { BASE_URL } from 'util/requests';
+import ProductInfoLoader from './ProductInfoLoader';
+import ProductDetailLoader from './ProductDetailLoader';
 
 type UrlParams = {
   productId: string;
-}
+};
 
 const ProductDetails = () => {
-
   const { productId } = useParams<UrlParams>();
-
+  const [isLoading, setIsLoading] = useState(false);
   const [product, setProduct] = useState<Product>();
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/products/${productId}`)
-      .then( response => {
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/products/${productId}`)
+      .then((response) => {
         setProduct(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [productId]);
-
 
   return (
     <div className="product-details-container">
@@ -36,24 +41,29 @@ const ProductDetails = () => {
         </Link>
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container">
-              <img
-                src={product?.imgUrl}
-                alt={product?.name}
-              />
-            </div>
-            <div className="name-price-container">
-              <h1>{product?.name}</h1>
-              { product && <ProductPrice price={product?.price} />}
-            </div>
+            {isLoading ? (
+              <ProductInfoLoader />
+            ) : (
+              <>
+                <div className="img-container">
+                  <img src={product?.imgUrl} alt={product?.name} />
+                </div>
+                <div className="name-price-container">
+                  <h1>{product?.name}</h1>
+                  {product && <ProductPrice price={product?.price} />}
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="description-container">
-              <h2>Descrição do produto</h2>
-              <p>
-                {product?.description}
-              </p>
-            </div>
+            {isLoading ? (
+              <ProductDetailLoader />
+            ) : (
+              <div className="description-container">
+                <h2>Descrição do produto</h2>
+                <p>{product?.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
