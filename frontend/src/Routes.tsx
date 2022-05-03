1f332f7789911a9bcdcf9a029cf273566a3d2499
Jean Marcel <jean.msv@gmail.com>
2022-05-03 18:11:02 -0300

    Page admin auth

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,6 +4,8 @@ import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
 import ProductDetails from 'pages/ProductDetails';
+import Auth from 'pages/Admin/Auth';
+import NavbarAdmin from 'pages/Admin/Navbar';
 
 const Caminho = () => (
   <BrowserRouter>
@@ -12,11 +14,54 @@ const Caminho = () => (
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Catalog />} />
       <Route path="/products/:productId" element={<ProductDetails />} />
-      <Route path="/admin" element={<Navigate replace to="/admin/products" />} />
+      <Route
+        path="/admin"
+        element={<Navigate replace to="/admin/products" />}
+      />
       <Route path="/admin" element={<Admin />}>
-        <Route path='products' element={<h1>Product CRUD</h1>} />
-        <Route path='categories' element={<h1>Category CRUD</h1>} />
-        <Route path='users' element={<h1>User CRUD</h1>} />
+        <Route
+          path="products"
+          element={
+            <div className="admin-container">
+              <NavbarAdmin />
+              <div className="admin-content">
+                <h1>Product CRUD</h1>
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="categories"
+          element={
+            <div className="admin-container">
+              <NavbarAdmin />
+              <div className="admin-content">
+                <h1>Category CRUD</h1>
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="users"
+          element={
+            <div className="admin-container">
+              <NavbarAdmin />
+              <div className="admin-content">
+                <h1>User CRUD</h1>
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="auth"
+          element={
+            <div className="admin-container">
+              <div className="admin-content">
+                <Auth />
+              </div>
+            </div>
+          }
+        />
       </Route>
     </Routes>
   </BrowserRouter>
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
-import Navbar from './Navbar';
+//import Navbar from './Navbar';
 
 import './style.css';
 
 const Admin = () => {
   return (
-    <div className="admin-container">
+    /*<div className="admin-container">
       <Navbar />
       <div className="admin-content">
         <Outlet />
       </div>
-    </div>
+    </div> */
+
+    <Outlet />
   );
 };
 
