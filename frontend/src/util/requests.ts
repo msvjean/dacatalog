7ae94b20f2460b138bb970f0d84361f09a4b1157
Jean Marcel <jean.msv@gmail.com>
2022-05-11 17:35:34 -0300

    Login/Logout logic

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -14,7 +14,7 @@ const tokenKey = 'authData';
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
-type TokenData = {
+export type TokenData = {
   exp: number;
   user_name: string;
   authorities: Role[];
@@ -73,6 +73,10 @@ export const getAuthData = () => {
   const str = localStorage.getItem(tokenKey) ?? '{}';
   return JSON.parse(str) as LoginResponse;
 };
+
+export const removeAuthData = () => {
+  localStorage.removeItem(tokenKey);
+}
 
 // Add a request interceptor
 axios.interceptors.request.use(
