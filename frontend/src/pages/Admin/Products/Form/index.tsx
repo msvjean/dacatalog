f6f4f609e458188c50a7be703ffa061f4ba74339
Jean Marcel <jean.msv@gmail.com>
2022-05-26 10:46:01 -0300

    ProductCrudForm html

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,9 +1,25 @@
-
+import './styles.css';
 
 const Form = () => {
-    return (
-        <h1>Cadastro de produto</h1>
-    );
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h3 className="product-crud-form-title">DADOS DO PRODUTO</h3>
+        <form action="">
+          <div className="row">
+            <div className="col-md-6">
+              <input type="text" className="form-control base-input" />
+              <input type="text" className="form-control base-input" />
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="col-md-6">
+              <textarea name="" rows={10} className="form-control base-input"></textarea>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
