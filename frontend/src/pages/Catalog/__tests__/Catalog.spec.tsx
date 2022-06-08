02ddcd960cb81ab89cb3604eceb7fdd6fcf7e7c5
Jeanmsv <jean.msv@gmail.com>
2022-06-08 14:16:00 -0300

    MSW mock server, fixtures

--- frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import Catalog from '..';
 import history from 'util/history';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Catalog tests', () => {
 
@@ -12,11 +17,11 @@ describe('Catalog tests', () => {
                 <Catalog />
             </Router>
         );
-        
+
         expect(screen.getByText("Catálogo de produtos")).toBeInTheDocument();
 
         await waitFor(() => {
-            expect(screen.getByText("Smart TV")).toBeInTheDocument();
+            expect(screen.getByText("Macbook Pro")).toBeInTheDocument();
         });
     });
 
