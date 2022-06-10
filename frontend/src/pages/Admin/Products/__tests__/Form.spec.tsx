8396b1d34b62450215cd8d3c570d6b487e1710bf
Jeanmsv <jean.msv@gmail.com>
2022-06-10 16:41:02 -0300

    test update should show toast and redirect when submit form correctly

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import Form from '../Form';
 import { Router, useParams } from 'react-router-dom';
 import history from 'util/history';
-import { server } from './fixtures';
+import { productResponse, server } from './fixtures';
 import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 
@@ -22,13 +22,13 @@ describe('Product form create tests', () => {
         (useParams as jest.Mock).mockReturnValue({
             productId: 'create'
         })
-        
+
     });
 
     jest.setTimeout(30000);
 
     test('should show toast and redirect when submit form correctly', async () => {
-        
+
         render(
             <Router history={history}>
                 <ToastContainer />
@@ -48,45 +48,45 @@ describe('Product form create tests', () => {
         userEvent.type(priceInput, "100.51");
         userEvent.type(imgUrlInput, "https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg");
         userEvent.type(descriptionInput, "Mouse ultra rapido");
-        
+
         userEvent.click(submitButton);
 
         waitFor(() => {
             const toastElemet = screen.getByText('Produto cadastrado com sucesso');
             expect(toastElemet).toBeInTheDocument();
-            expect(history.location.pathname).toEqual('/admin/products');           
-          });
+            expect(history.location.pathname).toEqual('/admin/products');
+        });
     });
 
     test('should show 5 validation messages when just clicking submit', async () => {
-        
+
         render(
             <Router history={history}>
                 <Form />
             </Router>
         );
-        
+
         const submitButton = screen.getByRole('button', { name: /salvar/i });
-        
+
         userEvent.click(submitButton);
 
         await waitFor(() => {
             const messages = screen.getAllByText('Campo obrigatório');
             expect(messages).toHaveLength(5);
         });
-        
+
     });
 
     test('should clear validation massages when filling out the form correctly', async () => {
-        
+
         render(
             <Router history={history}>
                 <Form />
             </Router>
         );
-        
+
         const submitButton = screen.getByRole('button', { name: /salvar/i });
-        
+
         userEvent.click(submitButton);
 
         await waitFor(() => {
@@ -105,11 +105,60 @@ describe('Product form create tests', () => {
         userEvent.type(priceInput, "100.51");
         userEvent.type(imgUrlInput, "https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg");
         userEvent.type(descriptionInput, "Mouse ultra rapido");
-        
-        await waitFor(() => {
+
+        waitFor(() => {
             const messages = screen.queryAllByText('Campo obrigatório');
             expect(messages).toHaveLength(0);
         });
     });
 
+});
+
+describe('Product form update tests', () => {
+
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: '2'
+        })
+
+    });
+
+    jest.setTimeout(30000);
+
+    test('should show toast and redirect when submit form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+
+        waitFor(() => {
+            const nameInput = screen.getByTestId("name");
+            const priceInput = screen.getByTestId("price");
+            const imgUrlInput = screen.getByTestId("imgUrl");
+            const descriptionInput = screen.getByTestId("description");
+
+            const fomElement = screen.getByTestId("form");
+
+            expect(nameInput).toHaveValue(productResponse.name);
+            expect(priceInput).toHaveValue(String('R$ ' + productResponse.price));
+            expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+            expect(descriptionInput).toHaveValue(productResponse.description);
+
+            const ids = productResponse.categories.map(item => String(item.id));
+            expect(fomElement).toHaveFormValues({categories: ids});
+        });
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+        userEvent.click(submitButton);
+
+        waitFor(() => {
+            const toastElemet = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElemet).toBeInTheDocument();
+            expect(history.location.pathname).toEqual('/admin/products');
+        });
+    });
 });
--- frontend/src/pages/Admin/Products/__tests__/fixtures.ts
+++ frontend/src/pages/Admin/Products/__tests__/fixtures.ts
@@ -44,14 +44,18 @@ const findCategoriesResponse = {
     "empty": false
 }
 
-const saveProductResponse = {
-    "id": 4,
-    "name": "PC Gamer",
+export const productResponse = {
+    "id": 2,
+    "name": "Smart TV",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "price": 1200.0,
-    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg",
+    "price": 2190.0,
+    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
     "date": "2020-07-14T10:00:00Z",
     "categories": [
+        {
+            "id": 2,
+            "name": "Eletrônicos"
+        },
         {
             "id": 3,
             "name": "Computadores"
@@ -68,8 +72,20 @@ export const server = setupServer(
     }),
     rest.post(`${BASE_URL}/products`, (req, res, ctx) => {
         return res(
-            ctx.json(saveProductResponse),
+            ctx.json(productResponse),
             ctx.status(201),
+        )
+    }),
+    rest.put(`${BASE_URL}/products/:productId`, (req, res, ctx) => {
+        return res(
+            ctx.json(productResponse),
+            ctx.status(200),
+        )
+    }),
+    rest.get(`${BASE_URL}/products/:productId`, (req, res, ctx) => {
+        return res(
+            ctx.json(productResponse),
+            ctx.status(200),
         )
     })
 );
